c676ab5c1e52819b933ed048b99977e8c7cdaee7
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2021-11-23 16:27:24 -0300

    wip.

--- postgres.sql
+++ postgres.sql
@@ -1,11 +1,14 @@
 create table customers (
-	uidpk serial,
- 	created_date varchar(20) default to_char(current_timestamp,'YYYY-MM-DD HH:MI:SS'),
-	email varchar(255) unique not null,
-	primary key(uidpk)
+  uidpk serial,
+  created_date varchar(20) default to_char(current_timestamp,'YYYY-MM-DD HH:MI:SS'),
+  email varchar(255) unique not null,
+  primary key(uidpk)
 );
 
-insert into customers (email) values('hodor@'||random())
+alter table customers replica identity full;
+
+insert into customers (email) values('hodor@'||random());
+insert into customers (email) values('hodor@'||random());
 
 select * from customers
 
@@ -20,13 +23,20 @@ create table orders (
   constraint customer_fk foreign key(customer_uid) references customers(uidpk) on delete set null
 );
 
-insert into orders (total, status, order_number, customer_uid) 
-values (100*random(), 'COMPLETED', (100000*random())::integer, 2)
+alter table orders replica identity full;
 
-select * from orders
+insert into orders (total, status, order_number, customer_uid) 
+values (100*random(), 'COMPLETED', (100000*random())::integer, 1);
+
+insert into orders (total, status, order_number, customer_uid) 
+values (100*random(), 'COMPLETED', (100000*random())::integer, 2);
+
+select * from orders;
 
 create table orders_agg (
   customer_uid integer,
   total decimal(19,2) default null,
   primary key(customer_uid)
 );
+
+select * from orders_agg;
---
+++ readme.md
@@ -0,0 +1,4 @@
+### sources:
+
+- https://noti.st/morsapaes/liQzgs#s9kzejc
+- https://github.com/ververica/flink-sql-CDC
